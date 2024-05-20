70d6f56ef2dbfe5f2740ed52f85d4df2717f6b40
Agnieszka-Dzwolak <dzwolak.agnieszka@gmail.com>
2024-05-20 14:13:33 +0200

    add responsive and readme

--- src/events/enterEvent.js
+++ src/events/enterEvent.js
@@ -1,7 +1,7 @@
 import dom from '../dom.js';
 import getPokemonHandler from '../handlers/getPokemonHandler.js';
 
-const enterEvent = (e) => {
+const enterEvent = () => {
     dom.input.addEventListener('keyup', (e) => {
         if (e.key === 'Enter') {
             getPokemonHandler();
